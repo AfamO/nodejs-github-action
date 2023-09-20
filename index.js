d12a9ab709ec6cec87f142da00550d0fb4cf5e2f
Afam Okonkwo <AfamO>
2023-09-20 11:43:58 +0100

    my codes

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello World, Testing GitHub Action");
+const multiplier = (a,b)=>{return a * b };
+console.log("My multiplier result=="+multiplier(4,5))
---
+++ tests.js
@@ -0,0 +1,7 @@
+const multiplier = (a,b)=>{return a * b };
+if(multiplier(4,5) == 20)
+	console.log("Test 1 Passed");
+if(multiplier(4,4) == 16)
+	console.log("Test 2 Passed");
+if(multiplier(10,10) == 100)
+	console.log("Test 3 Passed");
